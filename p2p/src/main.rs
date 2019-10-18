ced1669ff1d989acb0db52cca769d6d541ffd5b4
Jethro Shuwen Sun <jethrosun@css.neu.edu>
2019-10-18 10:34:46 -0400

    Changed files w/ little update

--- p2p/src/main.rs
+++ p2p/src/main.rs
@@ -1,26 +1,25 @@
 #![allow(dead_code)]
 
+extern crate hyper;
 extern crate rand;
 extern crate sha1;
-extern crate hyper;
 #[macro_use]
 extern crate log;
 
 pub mod bencode;
-pub mod torrent;
 pub mod downloader;
 pub mod storage;
+pub mod torrent;
 
+use downloader::Downloader;
+use log::{LogLevel, LogMetadata, LogRecord, SetLoggerError};
+use std::env;
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
-use std::env;
-use log::{LogRecord, LogLevel, LogMetadata, SetLoggerError};
-
-use torrent::Torrent;
-use downloader::Downloader;
 use storage::memory::MemoryStorage;
 use storage::partial::PartialStorage;
+use torrent::Torrent;
 
 fn main() {
     Logger::init().expect("Failed to initialize logger");
@@ -35,12 +34,12 @@ fn main() {
     };
 
     println!("Torrent file: {}", path);
-    
+
     let (torrent, info_hash) = read_torrent_file(path.clone()).unwrap();
-    
+
     println!("Parsed file!");
     println!("Downloading: {:?}", torrent.info.root);
-    
+
     let mut downloader: Downloader<PartialStorage<MemoryStorage>> =
         Downloader::new(info_hash, torrent.clone());
 
@@ -53,7 +52,8 @@ fn main() {
 fn read_torrent_file<P: AsRef<Path>>(path: P) -> Option<(Torrent, [u8; 20])> {
     let mut file = File::open(path).expect("failed to open file");
     let mut contents = Vec::new();
-    file.read_to_end(&mut contents).expect("failed to read file");
+    file.read_to_end(&mut contents)
+        .expect("failed to read file");
 
     let bvalue = match bencode::decode(&contents) {
         Ok(x) => x,
@@ -78,19 +78,27 @@ fn split_to_files<P: AsRef<Path>>(source: P, torrent: Torrent) {
     let mut source = File::open(source).expect("failed to open source file");
     use std::io::prelude::*;
     let mut data = Vec::new();
-    source.read_to_end(&mut data).expect("failed to read source");
+    source
+        .read_to_end(&mut data)
+        .expect("failed to read source");
     let mut start = 0_usize;
     for file in torrent.info.files.into_iter() {
         let mut dest = File::create(file.path.clone()).expect("failed to create file");
         let end = start + file.length as usize;
-        println!("interval {} - {} goes to {:?}", start, end, file.path.clone());
+        println!(
+            "interval {} - {} goes to {:?}",
+            start,
+            end,
+            file.path.clone()
+        );
         dest.write_all(&data[start..end]).expect("failed to write");
         start = end;
     }
     println!("wrote total {} bytes", start);
 }
 
-const LOGGING_LEVEL: LogLevel = LogLevel::Debug;
+// const LOGGING_LEVEL: LogLevel = LogLevel::Debug;
+const LOGGING_LEVEL: LogLevel = LogLevel::Error;
 struct Logger;
 
 impl log::Log for Logger {
