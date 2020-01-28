963bfbece05ebb49c7e4b972fa9136b2cdaa49e9
Jethro Shuwen Sun <jethro.s.sun@gmail.com>
2020-01-28 18:22:50 -0500

    transcoder done

--- transcoder/src/main.rs
+++ transcoder/src/main.rs
@@ -7,18 +7,16 @@ use std::env;
 use std::fs::File;
 use std::io;
 
-/// Src media can be found from https://media.xiph.org/video/derf/y4m/
-fn main() {
+fn transcode(infile: String, outfile: String, width_height: String) {
     let args: Vec<_> = env::args().collect();
-    if args.len() != 4 {
-        return println!("Usage: {} in.y4m WxH out.y4m", args[0]);
-    }
 
-    let mut infh: Box<dyn io::Read> = if args[1] == "-" {
-        Box::new(io::stdin())
-    } else {
-        Box::new(File::open(&args[1]).unwrap())
-    };
+    let mut infh: Box<dyn io::Read> = Box::new(File::open(&infile).unwrap());
+    let mut outfh: Box<dyn io::Write> = Box::new(File::create(&outfile).unwrap());
+    let dst_dims: Vec<_> = width_height
+        .split("x")
+        .map(|s| s.parse().unwrap())
+        .collect();
+
     let mut decoder = y4m::decode(&mut infh).unwrap();
 
     if decoder.get_bit_depth() != 8 {
@@ -28,16 +26,10 @@ fn main() {
         );
     }
     let (w1, h1) = (decoder.get_width(), decoder.get_height());
-    let dst_dims: Vec<_> = args[2].split("x").map(|s| s.parse().unwrap()).collect();
     let (w2, h2) = (dst_dims[0], dst_dims[1]);
     let mut resizer = resize::new(w1, h1, w2, h2, Gray8, Triangle);
     let mut dst = vec![0; w2 * h2];
 
-    let mut outfh: Box<dyn io::Write> = if args[3] == "-" {
-        Box::new(io::stdout())
-    } else {
-        Box::new(File::create(&args[3]).unwrap())
-    };
     let mut encoder = y4m::encode(w2, h2, decoder.get_framerate())
         .with_colorspace(y4m::Colorspace::Cmono)
         .write_header(&mut outfh)
@@ -55,4 +47,18 @@ fn main() {
             _ => break,
         }
     }
+}
+
+/// src media can be found at:
+/// https://media.xiph.org/video/derf/y4m/
+fn main() {
+    let infile = "tiny.y4m";
+    let outfile = "out.y4m";
+    let width_height = "360x24";
+
+    transcode(
+        infile.to_string(),
+        outfile.to_string(),
+        width_height.to_string(),
+    );
 }
