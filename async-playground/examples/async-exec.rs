76ba758fd13e1514bf93eccc5abaa6aa400a21ff
jethrosun <jethro.s.sun@gmail.com>
2020-05-24 00:05:48 -0400

    Added async playground and core affinity examples

---
+++ async-playground/examples/async-exec.rs
@@ -0,0 +1,90 @@
+use futures::future::{BoxFuture, FutureExt};
+use futures::task::{waker_ref, ArcWake};
+use std::future::Future;
+use std::sync::mpsc::{sync_channel, Receiver, SyncSender};
+use std::sync::{Arc, Mutex};
+use std::task::{Context, Poll};
+use std::{thread, time};
+
+struct Executor {
+    ready_queue: Receiver<Arc<Task>>,
+}
+
+impl Executor {
+    fn run(&self) {
+        while let Ok(task) = self.ready_queue.recv() {
+            let mut future_slot = task.future.lock().unwrap();
+
+            if let Some(mut future) = future_slot.take() {
+                let waker = waker_ref(&task);
+                let context = &mut Context::from_waker(&*waker);
+
+                if let Poll::Pending = future.as_mut().poll(context) {
+                    *future_slot = Some(future)
+                }
+            }
+        }
+    }
+}
+
+#[derive(Clone)]
+struct Spawner {
+    task_sender: SyncSender<Arc<Task>>,
+}
+
+impl Spawner {
+    // TODO change Output type
+    fn spawn(&self, future: impl Future<Output = ()> + 'static + Send) {
+        let future = future.boxed();
+        let task = Arc::new(Task {
+            future: Mutex::new(Some(future)),
+            task_sender: self.task_sender.clone(),
+        });
+
+        self.task_sender.send(task).unwrap();
+    }
+}
+
+struct Task {
+    future: Mutex<Option<BoxFuture<'static, ()>>>,
+    task_sender: SyncSender<Arc<Task>>,
+}
+
+impl ArcWake for Task {
+    fn wake_by_ref(arc_self: &Arc<Self>) {
+        arc_self.task_sender.send(arc_self.clone()).unwrap();
+    }
+}
+
+fn new_executor_and_spawner() -> (Executor, Spawner) {
+    const MAX_QUEUED_TASKS: usize = 10_000;
+    let (task_sender, ready_queue) = sync_channel(MAX_QUEUED_TASKS);
+
+    (Executor { ready_queue }, Spawner { task_sender })
+}
+
+fn main() {
+    let (executor, spawner) = new_executor_and_spawner();
+
+    spawner.spawn(async {
+        println!("async task spawn!!");
+
+        // TODO Do slower processing
+        let future = async {
+            for _ in 1..1000 {
+                println!("loop");
+
+                thread::sleep(time::Duration::from_secs(2));
+            }
+        };
+
+        future.await;
+
+        println!("Done!");
+    });
+
+    println!("hoge");
+
+    drop(spawner);
+    executor.run();
+}
