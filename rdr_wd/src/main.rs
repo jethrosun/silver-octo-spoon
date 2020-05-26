f56c2a202623469c27b75866d506a981688f4898
jethrosun <jethro.s.sun@gmail.com>
2020-05-26 00:37:45 -0400

    Done

--- rdr_wd/src/main.rs
+++ rdr_wd/src/main.rs
@@ -16,53 +16,75 @@ pub fn rdr_load_workload(
     file_path: String,
     num_of_secs: usize,
     num_of_user: usize,
-) -> Result<HashMap<usize, HashMap<usize, Vec<(i32, String)>>>> {
+) -> Result<HashMap<usize, Vec<(u64, String, usize)>>> {
     // time in second, workload in that second
-    let mut workload =
-        HashMap::<usize, HashMap<usize, Vec<(i32, String)>>>::with_capacity(num_of_secs);
+    // let mut workload = HashMap::<usize, HashMap<usize, Vec<(u64, String)>>>::with_capacity(num_of_secs);
+    let mut workload = HashMap::<usize, Vec<(u64, String, usize)>>::with_capacity(num_of_secs);
 
     let file = File::open(file_path).expect("file should open read only");
     let json_data: Value = from_reader(file).expect("file should be proper JSON");
+    // println!("{:?}", json_data);
 
     for sec in 0..num_of_secs {
         // println!("sec {:?}", sec,);
         // user, workload for that user
-        let mut sec_wd = HashMap::<usize, Vec<(i32, String)>>::with_capacity(100);
+        //
+        // sec = 266
+        // sec_wd = {'86': [39, 'thumb.brandreachsys.com'], '23': [42, 'facebook.com'], '84': [86, 'ynetnews.com'], '33': [284, 'techbargains.com'], '9': [309, 'bing.com'], '76': [357, 'eventful.com'], '43': [365, 'ad.yieldmanager.com'], '63': [468, 'ads.brazzers.com'], '72': [520, 'sidereel.com'], '57': [586, 'daum.net'], '81': [701, 'target.com'], '95': [732, 'lezhin.com'], '88': [802, 'nba.com'], '49': [827, 'web4.c2.cyworld.com'], '27': [888, 'hv3.webstat.com'], '98': [917, 'youtube.com']}
+        // let mut sec_wd = HashMap::<usize, Vec<(u64, String)>>::with_capacity(100);
+        // we keep track of the millisecond appeared
+        let mut millis: Vec<(u64, String, usize)> = Vec::new();
 
+        // println!("{:?} {:?}", sec, json_data.get(sec.to_string()));
         let urls_now = match json_data.get(sec.to_string()) {
-            Some(val) => val.as_array(),
+            Some(val) => val,
             None => continue,
         };
-        let all_seq = match urls_now {
-            Some(v) => v,
-            None => continue,
-        };
-
-        for seq in all_seq {
-            let visits = seq.as_array().unwrap();
-            // println!("\n sec {:?}, break: {:?}", sec, visits);
-
-            let mut vec_wd = Vec::new();
-            for idx in 0..visits.len() {
-                let time_url = visits[idx].as_str().unwrap().to_string();
-                let v: Vec<&str> = time_url.split(':').collect();
-                let wait_time: i32 = v[1].parse().unwrap();
-                vec_wd.push((wait_time, v[0].to_string()));
-            }
-            let magic = get_magic_num(num_of_user);
-            if sec == 599 {
-                println!("\n{:?} {:?}", magic, vec_wd);
-            }
-            sec_wd.insert(magic, vec_wd);
+        // println!("{:?}", urls_now);
+        for user in 0..num_of_user {
+            let urls = match urls_now.get(user.to_string()) {
+                Some(val) => val.as_array(),
+                None => continue,
+            };
+            // println!("{:?}", urls.unwrap());
+            millis.push((
+                urls.unwrap()[0].as_u64().unwrap(),
+                urls.unwrap()[1].as_str().unwrap().to_string(),
+                user,
+            ));
         }
-        workload.insert(sec, sec_wd);
+        millis.sort();
+        // sec_wd.insert(millis);
+
+        // {'96': [53, 'video.od.visiblemeasures.com'],
+        //  '52': [104, 'drift.qzone.qq.com'],
+        //  '15': [153, 'club.myce.com'],
+        //  '78': [180, 'ad.admediaprovider.com'],
+        //  '84': [189, 'inkido.indiana.edu'],
+        //  '34': [268, 'waterjet.net'],
+        //  '97': [286, 'apple.com'],
+        //  '6': [317, 'southparkstudios.com'],
+        //  '14': [362, 'en.wikipedia.org'],
+        //  '27': [499, 'google.com'],
+        //  '42': [619, 'womenofyoutube.mevio.com'],
+        //  '75': [646, 'news.msn.co.kr'],
+        //  '30': [750, 'hcr.com'],
+        //  '61': [759, 'blogs.medicine.iu.edu'],
+        //  '70': [815, 'mini.pipi.cn'],
+        //  '54': [897, 'msn.foxsports.com'],
+        //  '29': [926, 'target.com']}
+        if sec == 599 {
+            println!("{:?}, ", millis);
+        }
+        workload.insert(sec, millis);
     }
     Ok(workload)
 }
 
 fn main() {
+    // "/home/jethros/dev/silver-octo-spoon/workload_tempaltes/rdr_workload.json".to_string(),
     let workload = rdr_load_workload(
-        "/home/jethros/dev/silver-octo-spoon/workload_tempaltes/rdr_workload.json".to_string(),
+        "/home/jethros/dev/pvn-utils/workload/rdr_pvn_workload.json".to_string(),
         600,
         100,
     )
