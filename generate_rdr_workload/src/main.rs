c4826ca1b08907adb74aa958ae9a2bf63de03c9c
jethrosun <jethro.sun7@gmail.com>
2020-07-20 16:34:02 -0400

    new results

--- generate_rdr_workload/src/main.rs
+++ generate_rdr_workload/src/main.rs
@@ -27,10 +27,10 @@ fn main() -> Fallible<()> {
 
     // "/home/jethros/dev/projects/silver-octo-spoon/workload_tempaltes/rdr_pvn_workload.json";
     // "/home/jethros/dev/silver-octo-spoon/workload_tempaltes/rdr_pvn_workload.json";
-    let workload_path = "/Users/jethros/dev/pvn/utils/workloads/rdr_pvn_workload.json";
+    // let workload_path = "/Users/jethros/dev/pvn/utils/workloads/rdr_pvn_workload.json";
     // "/Users/jethros/dev/pvn/utils/workloads/rdr_pvn_workloads/rdr_pvn_workload_1.json";
-    // let workload_path =
-    //     "/home/jethros/dev/pvn/utils/workloads/rdr_pvn_workloads/rdr_pvn_workload_1.json";
+    let workload_path =
+        "/home/jethros/dev/pvn/utils/workloads/rdr_pvn_workloads/rdr_pvn_workload_5.json";
 
     let num_of_users = 100;
     let num_of_secs = 600;
